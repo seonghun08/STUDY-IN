8808494e1db6ed47428236cf179037cd837d78b6
seonghun <68460507+seonghun08@users.noreply.github.com>
2022-12-06 23:10:29 +0900

    comment

---
+++ src/main/java/com/studyIn/modules/order/OrderService.java
@@ -0,0 +1,14 @@
+package com.studyIn.modules.order;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+
+}
